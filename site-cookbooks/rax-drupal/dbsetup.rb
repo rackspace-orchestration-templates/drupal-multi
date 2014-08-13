6edef059ecbf56a90ec8bf68ab1d8acd32a4d3a2
Philip Eatherington <philip.eatherington@rackspace.com>
2014-08-13 14:23:14 -0500

    Updates gitignore, first go at db setup

---
+++ site-cookbooks/rax-drupal/dbsetup.rb
@@ -0,0 +1,51 @@
+# coding: utf-8
+#
+# Cookbook Name:: rax-drupal
+# Recipe:: dbsetup
+#
+# Copyright 2014
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "mysql::client" unless platform_family?('windows') # No MySQL client on Windows
+
+db = node['drupal']['db']
+
+mysql_connection_info = {
+  :host     => db['host'],
+  :username => 'root',
+  :password => node['mysql']['server_root_password']
+}
+
+mysql_database db['database_name'] do
+  connection  mysql_connection_info
+  action      :create
+end
+
+mysql_database_user db['username'] do
+  connection    mysql_connection_info
+  password      db['database_password']
+  host          db['host']
+  database_name db['database_name']
+  action        :create
+end
+
+mysql_database_user db['username'] do
+  connection    mysql_connection_info
+  database_name db['database_name']
+  privileges    [:all]
+  host          "10.%"
+  action        :grant
+end
+
