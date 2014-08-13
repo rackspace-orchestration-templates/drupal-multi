e741457115b9742b02b82f70663cd2fcf1ab9da5
Brint O'Hearn <brint.ohearn@rackspace.com>
2014-08-13 18:16:06 -0500

    initializing tests

---
+++ test/fabric/database.py
@@ -0,0 +1,19 @@
+from fabric.api import env, task
+from envassert import detect, file, package, port, process, service
+
+
+@task
+def mysql():
+    env.platform_family = detect.detect()
+
+    packages = ["holland", "mysql-server-5.5"]
+    for pkg in packages:
+        assert package.installed(pkg)
+
+    assert port.is_listening(3306)
+    assert process.is_up("mysqld")
+
+    root_my_cnf = "/root/.my.cnf"
+    assert file.exists(root_my_cnf)
+    assert file.mode_is(root_my_cnf, 600)
+    assert file.owner_is(root_my_cnf, "root")
---
+++ test/fabric/drupal_web.py
@@ -0,0 +1,14 @@
+from fabric.api import env, run, task
+from envassert import detect, file, group, package, port, process, service, \
+    user
+
+
+@task
+def check():
+    env.platform_family = detect.detect()
+
+    assert package.installed("php5")
+    assert file.is_dir("/var/www/vhosts")
+    assert port.is_listening(80)
+    assert process.is_up("apache2")
+    assert service.is_enabled("apache2")
