df3f2da5c6b1eeadfefff571aceb74a6e4624586
Brint O'Hearn <brint.ohearn@rackspace.com>
2014-08-20 17:37:33 -0500

    updating tests to look for varnish

--- test/fabric/drupal_web.py
+++ test/fabric/drupal_web.py
@@ -8,7 +8,9 @@ def check():
     env.platform_family = detect.detect()
 
     assert package.installed("php5")
+    assert package.installed("varnish")
     assert file.is_dir("/var/www/vhosts")
     assert port.is_listening(80)
+    assert port.is_listening(8080)
     assert process.is_up("apache2")
     assert service.is_enabled("apache2")
