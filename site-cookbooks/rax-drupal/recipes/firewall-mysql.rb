ad1c10a26bb307b4ffcc56cb297344cf96188a30
Philip Eatherington <philip.eatherington@rackspace.com>
2014-09-12 14:25:57 -0500

    updates to just take out the looping work and make it match others for rhel

--- site-cookbooks/rax-drupal/recipes/firewall-mysql.rb
+++ site-cookbooks/rax-drupal/recipes/firewall-mysql.rb
@@ -18,12 +18,11 @@
 case node["platform_family"]
 when "rhel", "fedora"
   fwfile = "/etc/sysconfig/iptables"
-  %w( node['mysql']['port'] ).each do |port|
-    rule = "-I INPUT -p tcp -m tcp --dport #{port} -j ACCEPT"
-    execute "Adding iptables rule for #{port}" do
-      command "iptables #{rule}"
-      not_if "grep \"\\#{rule}\" #{fwfile}"
-    end
+  port = node['mysql']['port'].to_i
+  rule = "-I INPUT -p tcp -m tcp --dport #{port} -j ACCEPT"
+  execute "Adding iptables rule for #{port}" do
+    command "iptables #{rule}"
+    not_if "grep \"\\#{rule}\" #{fwfile}"
   end
   # Save iptables rules
   execute "Saving mysql iptables rule set" do
