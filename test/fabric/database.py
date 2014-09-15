5ff6d7c241506491e42314cd33d16f2daca12583
Philip Eatherington <philip.eatherington@rackspace.com>
2014-09-15 16:18:39 -0500

    updates to do some fancy footwork for RHEL/CENT

--- test/fabric/database.py
+++ test/fabric/database.py
@@ -6,7 +6,19 @@ from envassert import detect, file, package, port, process, service
 def mysql():
     env.platform_family = detect.detect()
 
-    packages = ["holland", "mysql-server-5.5"]
+    if env.platform_family == 'debian':
+        mysql_package = 'mysql-server-5.5'
+        mysql_process = 'mysql'
+        holland_package = 'holland'
+    elif env.platform_family == 'rhel':
+        mysql_package = 'mysql-server'
+        mysql_process = 'mysqld'
+        holland_package = 'holland'
+    else:
+        raise ValueError('OS ' + env.platform_family +
+                         ' unknown, update tests.')
+
+    packages = [holland_package, mysql_package]
     for pkg in packages:
         assert package.installed(pkg)
 
@@ -17,3 +29,5 @@ def mysql():
     assert file.exists(root_my_cnf)
     assert file.mode_is(root_my_cnf, 600)
     assert file.owner_is(root_my_cnf, "root")
+    assert process.is_up(mysql_process)
+    assert service.is_enabled(mysql_process)
