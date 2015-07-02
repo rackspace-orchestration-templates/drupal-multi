956e54a92efda430ff5af6e157b8ab66552a2fc3
Justin Phelps <linuturk@onitato.com>
2015-07-02 14:21:18 -0500

    cleaning up the fabric script

--- test/fabric/drupal_web.py
+++ test/fabric/drupal_web.py
@@ -1,45 +1,25 @@
-import re
-from fabric.api import env, run, task, hide
+from fabric.api import env, task
 from envassert import detect, file, package, port, process, service
 from hot.utils.test import get_artifacts, http_check
-
-
-def drupal_is_responding():
-    with hide('running', 'stdout'):
-        site = "http://localhost/"
-        homepage = run("wget --quiet --output-document - %s" % site)
-    if re.search('Welcome to Drupal7', homepage):
-        return True
-    else:
-        return False
 
 
 @task
 def check():
     env.platform_family = detect.detect()
 
+    site = "http://localhost/"
+    string = "Welcome to Drupal7"
+    apache_process = 'apache2'
+    php_package = 'php5'
+
     assert file.is_dir("/var/www/vhosts"), '/var/www/vhosts is wrong'
     assert port.is_listening(80), 'port 80 not listening'
     assert port.is_listening(8080), 'port 8080 not listening'
     assert package.installed("varnish"), 'varnish not installed'
-    assert drupal_is_responding(), 'Drupal did not respond as expected.'
-
-    apache_process = 'apache2'
-    php_package = 'php5'
-
-    if env.platform_family == 'debian':
-        apache_process = 'apache2'
-        php_package = 'php5'
-    elif env.platform_family == 'rhel':
-        apache_process = 'httpd'
-        php_package = 'php'
-    else:
-        raise ValueError('OS ' + env.platform_family +
-                         ' unknown, update tests.')
-
     assert package.installed(php_package), 'php not installed'
     assert process.is_up(apache_process), 'apache is not running'
     assert service.is_enabled(apache_process), 'apache is not enabled'
+    assert http_check(site, string), 'Drupal did not respond as expected.'
 
 
 @task
