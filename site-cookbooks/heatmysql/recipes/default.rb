963249170dc1d49d5b6627a0598866544e8e7f01
Randall Burt <randall.burt@rackspace.com>
2015-09-23 17:31:36 -0500

    Really fix grants and create database

--- site-cookbooks/heatmysql/recipes/default.rb
+++ site-cookbooks/heatmysql/recipes/default.rb
@@ -8,7 +8,7 @@ mysql_service 'default' do
   bind_address node[:mysql][:bind_address]
   initial_root_password node[:mysql][:server_root_password]
   socket '/var/run/mysqld/mysqld.sock'
-  action [:create, :start]
+  action [ :create, :start ]
 end
 
 mysql_connection_info = {
@@ -23,4 +23,17 @@ mysql_database_user 'root' do
   host '%'
   grant_option true
   action :grant
+end
+
+mysql_database node[:drupal][:db][:name]  do
+  connection mysql_connection_info
+  action :create
+end
+
+mysql_database_user node[:drupal][:db][:user] do
+  connection mysql_connection_info
+  password node[:drupal][:db][:pass]
+  database_name node[:drupal][:db][:name]
+  host node[:drupal][:db][:host]
+  action [ :create, :grant ]
 end
