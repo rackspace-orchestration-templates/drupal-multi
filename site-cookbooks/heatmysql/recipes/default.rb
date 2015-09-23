dfdbbd476313de97792c64b43447b7f59921bddf
Randall Burt <randall.burt@rackspace.com>
2015-09-23 12:15:22 -0500

    Bind mysql appropriately

--- site-cookbooks/heatmysql/recipes/default.rb
+++ site-cookbooks/heatmysql/recipes/default.rb
@@ -1,5 +1,6 @@
 mysql_service 'default' do
   port node['mysql']['port']
+  bind_address node['mysql']['bind_address']
   initial_root_password node['mysql']['server_root_password']
   action [:create, :start]
 end
